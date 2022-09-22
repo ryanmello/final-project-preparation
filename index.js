dc003e21eade43bffdc5b301894b68e34e4856b6
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-09-21 20:55:09 -0700

    api pull

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
 
-async function main(){
-    const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersData = await users.json();
-    console.log(usersData);
+async function main() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersData = await users.json();
+  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 main();
+
+function userHTML(user) {
+  return `<div class="user-card">
+        <div class="user-card__container">
+            <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+    </div>`;
+}
