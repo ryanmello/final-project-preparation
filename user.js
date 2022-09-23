7b88f3c6259b7b176bf6300c721bcd572edb3c6b
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-09-23 16:15:56 -0700

    page rotation

--- user.js
+++ user.js
@@ -1,5 +1,30 @@
 const postListEl = document.querySelector(".post-list");
 
+async function onSearchChange(event){
+  const id = event.target.value;
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+  const postsData = await posts.json();
+  console.log(postsData);
+
+  postListEl.innerHTML = postsData.map(
+      post => `<div class="post">
+    <div class="post__title">
+      ${post.title}
+    </div>
+    <p class="post__body">
+      ${post.body}
+    </p>
+    </div>
+    `
+    )
+    .join('');
+}
+
+
+
+
 async function main() {
   const id = localStorage.getItem("id");
   const posts = await fetch(
@@ -20,6 +45,7 @@ async function main() {
     `
     )
     .join('');
+    //20:40
 }
 
 main();
